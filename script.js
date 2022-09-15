3e4c33fd3d10716a437be24fb1a3e1877662780e
unknown <sazidahmednassir@gmail.com>
2022-09-15 01:38:28 -0700

    fixed bug in reducer

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ function counterReducer(state = initialState, action) {
   if (action.type === "increment") {
     return {
       ...state,
-      count: count.value + 1,
+      count: state.count + 1,
     };
   } else if (action.type === "decrement") {
     return {
       ...state,
-      count: count.value - 1,
+      count: state.count - 1,
     };
   } else {
     return state;
@@ -27,3 +27,24 @@ function counterReducer(state = initialState, action) {
 
 //create store
 const store = Redux.createStore(counterReducer);
+
+//render function
+const render = () => {
+  const state = store.getState;
+};
+
+store.subscribe(render);
+
+//increment event
+incrementbtn.addEventListener("click", () => {
+  store.dispatch({
+    type: "increment",
+  });
+});
+
+//decrement event
+decrementbtn.addEventListener("click", () => {
+  store.dispatch({
+    type: "decrement",
+  });
+});
