7b0a8d101e256da723116dc87f865f41600fdf3f
unknown <sazidahmednassir@gmail.com>
2022-09-15 01:06:03 -0700

    reducer function created

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const initialState = {
 };
 
 //reducer function
+function counterReducer(state = initialState, action) {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      count: count.value + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      count: count.value - 1,
+    };
+  } else {
+    return state;
+  }
+}
